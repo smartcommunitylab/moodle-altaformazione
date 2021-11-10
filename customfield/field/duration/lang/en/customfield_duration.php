311fccab102872ffa0d3b871068ef5b79b4ffefe
AlbanaCelepija <acelepija@fbk.eu>
2021-11-11 00:35:19 +0100

    feat(customfield)add new duration custom field

---
+++ customfield/field/duration/lang/en/customfield_duration.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Customfield text field plugin strings
+ *
+ * @package   customfield_duration
+ * @copyright 2018 Toni Barbera <toni@moodle.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['errordefaultvaluenotinlist'] = 'The default value must be one of the options from the list above.';
+$string['errornotenoughoptions'] = 'Please provide at least two options, with each on a new line.';
+$string['invalidoption'] = 'Invalid option selected';
+$string['menuoptions'] = 'Menu options (one per line)';
+$string['pluginname'] = 'Duration';
+$string['privacy:metadata'] = 'The Duration field type plugin doesn\'t store any personal data; it uses tables defined in core.';
+$string['specificsettings'] = 'Duration field settings';
+$string['units'] = 'Units';
+$string['wrong_type_value'] = "Wrong duration value. Value must be numeric";
+
